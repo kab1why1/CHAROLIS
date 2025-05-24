fadd4a46c86f00af4682c1b50dcb5174ec000d35
kab1 <kab1why@gmail.com>
2025-05-24 21:10:22 +0300

    add profile and profile edit

--- charolis/charolis/Migrations/20250524172150_add balance prop.Designer.cs
+++ charolis/charolis/Migrations/20250524172150_add balance prop.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using charolis.DAL;
@@ -11,9 +12,11 @@ using charolis.DAL;
 namespace charolis.Migrations
 {
     [DbContext(typeof(EfContext))]
-    partial class EfContextModelSnapshot : ModelSnapshot
+    [Migration("20250524172150_add balance prop")]
+    partial class addbalanceprop
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ charolis/charolis/Models/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace charolis.Models;
+
+public class ProfileViewModel
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public decimal Balance { get; set; }
+
+    // Optional: change password
+    public string NewPassword { get; set; }
+    public string ConfirmPassword { get; set; }
+}
